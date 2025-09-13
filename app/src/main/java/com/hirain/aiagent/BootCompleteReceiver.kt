55b036d1046075506c073819385fa797bc5e1ad1
unknown <jun.he@hirain.com>
2025-09-13 23:20:49 +0800

    update

--- app/src/main/java/com/hirain/aiagent/BootCompleteReceiver.kt
+++ app/src/main/java/com/hirain/aiagent/BootCompleteReceiver.kt
@@ -1,23 +1,3 @@
-/******************************************************************************
-/                              Copyright
-/------------------------------------------------------------------------------
-/    Copyright © 2023 Z-ONE Technology Co., Ltd.  All rights reserved.
-/
-/    This software is furnished under a license and may be used and copied
-/    only in accordance with the terms of such license and with the inclusion
-/    of the above copyright notice. This software or any other copies thereof
-/    may not be provided or otherwise made available to any other person.
-/    No title to and ownership of the software is hereby transferred.
-/
-/    The information in this software is subject to change without notice
-/    and should not be constructed as a commitment by
-/    Z-ONE Technology Co., Ltd.
-/
-/    Z-ONE Technology Co., Ltd assumes no responsibility for the use
-/    or reliability of its Software on equipment which is not supported by
-/    Z-ONE Technology Co., Ltd.
-/------------------------------------------------------------------------------
- *******************************************************************************/
 
 package com.hirain.aiagent
 
